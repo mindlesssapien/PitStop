42c098dcbadf615113b1ca5d23dae3c5a37fc79b
mindlesssapien <sapienix@proton.me>
2026-06-27 21:55:27 +0530

    implemented init function

--- libpit.py
+++ libpit.py
@@ -1,4 +1,5 @@
 import os
+from os.path import isdir
 import re
 import sys
 import configparser
@@ -35,3 +36,119 @@ def main(argv=sys.argv[1:]):
         case _ : print("bad command")
 
 
+class GitRepository():
+
+    worktree, gitdir, conf = None, None, None
+
+    def __init__(self, path, force = False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a git repository {path}")
+        
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+        
+        if not force:
+            ver = self.conf.get("core", "repositoryformatversion")
+            if ver != 0:
+                raise Exception(f"Unsupported repositoryfomartversion: {ver}")
+
+
+def repo_path(repo, *path):
+    return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+    path = repo_path(repo, *path)
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception(f"Not a directory {path}")
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+def repo_create(path):
+    """create new repo at path"""
+
+    repo = GitRepository(path, True)
+    # First, we make sure the path either doesn't exist or is an
+    # empty dir.
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(f"{path} is not a directory")
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception(f"{path} is not empty")
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    #.git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    ret.add_section("core")
+    ret.set("core", "repositoryfomartversion", "0") #1 means initial format but with extensions
+    ret.set("core", "filemode", "false") #filemode false disables the tracking of file mode changes in the repo
+    ret.set("core", "bare", "false") #false indicates repo has a worktree
+    #git also has a worktree parametere in the config but not implemented here
+
+    return ret
+
+argsp = argsubparsers.add_parser("init", help="Inititalize a new empty repo")
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+def cmd_init(args):
+    repo_create(args.path)
+
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:
+        if required:
+            raise Exception("not a git repository")
+        else:
+            return None
+
+    return repo_find(parent, required)
